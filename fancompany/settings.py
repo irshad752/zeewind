0a365b75ff4741c39494cfc7addbefeedb2fcb03
Irshad <irshukkt211@gmail.com>
2026-02-20 10:15:11 +0530

    fix BASE_DIR and template/static/media paths

--- fancompany/settings.py
+++ fancompany/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'fancompany.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR.parent, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR.parent, "static")
+    os.path.join(BASE_DIR, "static")
 ] # points to faan_website/static 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") # where static files will be collected
 
@@ -134,4 +134,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") # where static files will be
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR.parent, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
