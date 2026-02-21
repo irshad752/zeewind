a9914ec7b3531d5ec3781dc0e55efb27f0806721
Irshad <irshukkt211@gmail.com>
2026-02-21 17:30:01 +0530

    fixed templates path

--- fancompany/fancompany/settings.py
+++ fancompany/fancompany/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'fancompany.fancompany.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR.parent / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
