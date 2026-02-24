02707af0dc32d6179591b91a983dad6ecb59fb71
Irshad <irshukkt211@gmail.com>
2026-02-24 12:07:32 +0530

    updated settings for render postgressql + local sqlite fallback

--- fancompany/fancompany/settings.py
+++ fancompany/fancompany/settings.py
@@ -82,12 +82,23 @@ WSGI_APPLICATION = 'fancompany.fancompany.wsgi.application'
 
 import dj_database_url
 
-DATABASES = {
-    "default": dj_database_url.config(
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if os.environ.get("RENDER") == "True":
+    # Production on Render
+    DATABASES = {
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    # Local fallback (SQLite)
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
