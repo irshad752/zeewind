b7cd7c2e060d2782ba6d3b79c3a26f4537815794
Irshad <irshukkt211@gmail.com>
2026-02-24 13:11:41 +0530

    dup delete db

--- fancompany/fancompany/settings.py
+++ fancompany/fancompany/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY" , "local-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["zeewind.onrender.com", "localhost", "127.0.0.1"]  # Allow all hosts for development
 
@@ -95,7 +95,7 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+            "NAME": BASE_DIR / "db.sqlite3" ,
         }
     }
 
