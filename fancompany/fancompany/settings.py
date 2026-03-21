bd29dcdd8d58fa7c679c0d749673ce182b09571e
Irshad <irshukkt211@gmail.com>
2026-03-22 00:36:31 +0530

    added migrations for app

--- fancompany/fancompany/settings.py
+++ fancompany/fancompany/settings.py
@@ -93,7 +93,7 @@ if os.environ.get("RENDER") == "True":
         )
     }
 else:
-    # Local fallback (SQLite)
+    # Local fallback (SQLite)  
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
@@ -153,12 +153,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Use Cloudinary for media storage
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-# Cloudinary configuration
-import cloudinary
-cloudinary.config(  
-    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.environ.get("CLOUDINARY_API_KEY"),
-    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
-)
-
-
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET')
+}
